233eea53155b8cf46d44db4a5d7c7614dd6418c8
Jhan Gomez <jhangomez971@gmail.com>
2026-07-08 19:21:00 +0000

    Reduced I/O operations by aproximately 98%, added shebang, fixed logging, updated ReadMe and more fixes

--- 3-2-1-Sync-Done/json_control.py
+++ 3-2-1-Sync-Done/json_control.py
@@ -2,31 +2,34 @@ import json
 import logging
 import os
 from datetime import datetime
-logging.basicConfig(level=logging.WARNING, filename='manifest.log', format='%(asctime)s - %(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO, filename='loginfo.log', format='%(asctime)s - %(levelname)s: %(message)s',force=True)
 def json_writer(hash, path):
   list = {}
-  if os.path.exists(path):
+  if os.path.exists(path): #It was treating an empty manifest as corrupt, when not..
      with open(path, 'r') as f:
         try:
            list = json.load(f)
         except json.JSONDecodeError:
-           logging.error("ERROR: Manifest is corrupted, rebuilding...")
+           logging.error(f"Manifest is corrupted, rebuilding. Date and time: {datetime.now().isoformat()}")
      list.update(hash)
      with open(path, "w") as f:
         json.dump(list, f, indent=4)
-
+     logging.info(f"Successfully updated manifest with {len(hash)} entries.")
 def hash_compare(file_path, current_hash, manifest_path):
     if not os.path.exists(manifest_path):
         return "new" 
+    if os.path.getsize(manifest_path) == 0:
+        return "new"
     with open(manifest_path, "r") as f:
         try:
             manifest_data = json.load(f) 
         except json.JSONDecodeError:
-            return "manifest_error"
+            return "new"
     if file_path not in manifest_data:
         return "new"
     stored_hash = manifest_data[file_path]["hash"]
     if current_hash == stored_hash:
+        print(f"Current hash: {current_hash}, Old: {stored_hash}")
         return "same"
     else:
         return "corrupted"
