7a5c9d5aa97065206ea8d3fedb809e9d55560050
Jhan Gomez <jhangomez971@gmail.com>
2026-07-10 09:33:41 -0400

    Reduced I/O operations again, test took 2 hours instead of 12, a 142.86% improvement but I will add more optimizations to get it under 30 minutes

--- 3-2-1-Sync-Done/json_control.py
+++ 3-2-1-Sync-Done/json_control.py
@@ -2,8 +2,11 @@ import json
 import logging
 import os
 from datetime import datetime
+val = 0
 logging.basicConfig(level=logging.INFO, filename='loginfo.log', format='%(asctime)s - %(levelname)s: %(message)s',force=True)
 def json_writer(hash, path):
+  global val
+  val = val + len(hash)
   list = {}
   if os.path.exists(path): #It was treating an empty manifest as corrupt, when not..
      with open(path, 'r') as f:
@@ -14,7 +17,8 @@ def json_writer(hash, path):
      list.update(hash)
      with open(path, "w") as f:
         json.dump(list, f, indent=4)
-     logging.info(f"Successfully updated manifest with {len(hash)} entries.")
+     if val % 2048 == 0:
+         logging.info(f"Successfully updated manifest with {len(hash)} entries.")
 def hash_compare(file_path, current_hash, manifest_path):
     if not os.path.exists(manifest_path):
         return "new" 
@@ -29,7 +33,6 @@ def hash_compare(file_path, current_hash, manifest_path):
         return "new"
     stored_hash = manifest_data[file_path]["hash"]
     if current_hash == stored_hash:
-        print(f"Current hash: {current_hash}, Old: {stored_hash}")
         return "same"
     else:
         return "corrupted"
